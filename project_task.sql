6a0549bd87e5aa34f20d802c8f18c02ccf42e9d9
hacker-194 <luitelkhagendra075@gmail.com>
2026-05-10 12:51:04 +0545

    Update project_task.sql

--- project_task.sql
+++ project_task.sql
@@ -54,4 +54,37 @@ join issued_status as ist
 on b.isbn = ist.issued_book_isbn
 group by 1;
 
+-- List Members Who Registered in the Last 180 Days:
+select * from members
+where reg_date >= current_date - interval '180 days';
+
+-- task 10 List Employees with Their Branch Manager's Name and their branch details:
+SELECT 
+    e.emp_id,
+    e.emp_name AS employee_name,
+    b.branch_id,
+    b.branch_adress,
+    b.contact_no,
+    e1.emp_name AS manager_name
+FROM employees AS e
+JOIN branch AS b
+    ON e.branch_id = b.branch_id
+JOIN employees AS e1
+    ON e1.emp_id = b.manager_id;
+
+-- Task 11. Create a Table of Books with Rental Price Above a Certain Threshold 7USD:
+create table books_price_above_seven
+as select * from books
+where rental_price > 7;
+
+-- Task 12: Retrieve the List of Books Not Yet Returned
+SELECT 
+    DISTINCT ist.issued_book_name
+FROM issued_status as ist
+LEFT JOIN
+return_status as rs
+ON ist.issued_id = rs.issued_id
+WHERE rs.return_id IS NULL
+SELECT * FROM return_status;
+
 
